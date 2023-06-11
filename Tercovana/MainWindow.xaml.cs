5f510b8abd40e12e0bf5d2f45a6f1aeaf1651b5d
KubaVy <kubik.vykydal@seznam.cz>
2023-06-11 19:32:47 +0200

    Přidejte soubory projektu.

---
+++ Tercovana/MainWindow.xaml.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TerceApp
+{
+    public partial class MainWindow : Window
+    {
+        private Random random;
+        private List<Terč> terce;
+        private int skore;
+        private int naboje;
+        private readonly int maxNaboje = 10;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            random = new Random();
+            terce = new List<Terč>();
+            skore = 0;
+            naboje = maxNaboje;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            StartGeneratorCasovac();
+            UpdateSkore();
+            UpdateNaboje();
+        }
+
+        private void StartGeneratorCasovac()
+        {
+            var generatorCasovac = new DispatcherTimer();
+            generatorCasovac.Tick += GeneratorCasovac_Tick;
+            generatorCasovac.Interval = TimeSpan.FromSeconds(2);
+            generatorCasovac.Start();
+        }
+
+        private void GeneratorCasovac_Tick(object sender, EventArgs e)
+        {
+            var velikost = random.Next(20, 100);
+            var umisteni = GetRandomUmisteni(velikost);
+            var terc = new Terč(velikost, umisteni);
+            terce.Add(terc);
+            canvas.Children.Add(terc.GetGrafika());
+
+            terc.Zavre += Terce_Zavre;
+            terc.Zasah += Terce_Zasah;
+
+            var zaviraciCasovac = new DispatcherTimer();
+            zaviraciCasovac.Tick += (s, args) =>
+            {
+                terc.Kliknuto();
+            };
+            zaviraciCasovac.Interval = TimeSpan.FromSeconds(2);
+            zaviraciCasovac.Start();
+        }
+
+        private void Terce_Zavre(object sender, EventArgs e)
+        {
+            var terc = (Terč)sender;
+            terce.Remove(terc);
+            canvas.Children.Remove(terc.GetGrafika());
+            UpdateSkore();
+        }
+
+        private void Terce_Zasah(object sender, EventArgs e)
+        {
+            skore++;
+            UpdateSkore();
+        }
+
+        private void UpdateSkore()
+        {
+            skoreTextBlock.Text = $"Skóre: {skore}";
+        }
+
+        private void UpdateNaboje()
+        {
+            nabojeTextBlock.Text = $"Náboje: {naboje}";
+
+            if (naboje == 0)
+            {
+                canvas.IsEnabled = false;
+            }
+            else
+            {
+                canvas.IsEnabled = true;
+            }
+        }
+
+        private Point GetRandomUmisteni(double velikost)
+        {
+            var maxX = canvas.ActualWidth - velikost;
+            var maxY = canvas.ActualHeight - velikost;
+            var x = random.NextDouble() * maxX;
+            var y = random.NextDouble() * maxY;
+            return new Point(x, y);
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            if (naboje > 0)
+            {
+                var bod = e.GetPosition(canvas);
+                bool trefa = false;
+
+                foreach (var terc in terce)
+                {
+                    if (terc.Zasah2(bod))
+                    {
+                        terc.Kliknuto();
+                        trefa = true;
+                        break;
+                    }
+                }
+
+                if (!trefa)
+                {
+                    skore--;
+                    UpdateSkore();
+                }
+
+                naboje--;
+                UpdateNaboje();
+            }
+        }
+
+        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            naboje = maxNaboje;
+            UpdateNaboje();
+        }
+    }
+}
---
+++ Tercovana/Terc.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TerceApp
+{
+    public class Terč
+    {
+        private double velikost;
+        private Point umisteni;
+        private Canvas skupinaElips;
+
+        public event EventHandler Zavre;
+        public event EventHandler Zasah;
+
+        public Terč(double velikost, Point umisteni)
+        {
+            this.velikost = velikost;
+            this.umisteni = umisteni;
+            skupinaElips = new Canvas();
+
+            var elips1 = CreateElips(velikost, Brushes.Red);
+            skupinaElips.Children.Add(elips1);
+
+            var elips2 = CreateElips(velikost * 0.75, Brushes.White);
+            var posun = (velikost - elips2.Width) / 2;
+            Canvas.SetLeft(elips2, posun);
+            Canvas.SetTop(elips2, posun);
+            skupinaElips.Children.Add(elips2);
+
+            var elips3 = CreateElips(velikost * 0.5, Brushes.Red);
+            posun = (velikost - elips3.Width) / 2;
+            Canvas.SetLeft(elips3, posun);
+            Canvas.SetTop(elips3, posun);
+            skupinaElips.Children.Add(elips3);
+
+            Canvas.SetLeft(skupinaElips, umisteni.X);
+            Canvas.SetTop(skupinaElips, umisteni.Y);
+
+            skupinaElips.MouseLeftButtonDown += SkupinaElips_MouseLeftButtonDown;
+        }
+
+        private Ellipse CreateElips(double velikost, Brush barva)
+        {
+            return new Ellipse
+            {
+                Width = velikost,
+                Height = velikost,
+                Fill = barva,
+                Stroke = Brushes.Black,
+                StrokeThickness = 2
+            };
+        }
+
+        private void SkupinaElips_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Kliknuto();
+            Zasah?.Invoke(this, EventArgs.Empty);
+        }
+
+        public UIElement GetGrafika()
+        {
+            return skupinaElips;
+        }
+
+        public bool Zasah2(Point bod)
+        {
+            var tercBod = new Point(umisteni.X + velikost / 2, umisteni.Y + velikost / 2);
+            var vzdalenost = Point.Subtract(bod, tercBod).Length;
+            return vzdalenost <= velikost / 2;
+        }
+
+        public void Kliknuto()
+        {
+            Zavre?.Invoke(this, EventArgs.Empty);
+        }
+
+        public void StartZaviraciCasovac()
+        {
+            var zaviraciCasovac = new DispatcherTimer();
+            zaviraciCasovac.Tick += ZaviraciCasovac_Tick;
+            zaviraciCasovac.Interval = TimeSpan.FromSeconds(8);
+            zaviraciCasovac.Start();
+        }
+
+        private void ZaviraciCasovac_Tick(object sender, EventArgs e)
+        {
+            Kliknuto();
+        }
+    }
+}
